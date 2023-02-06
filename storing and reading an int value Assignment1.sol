ebde66be645fdda5cfe9eb59cb9b1b1e5e8661b1
Rohith Sukka <rohithsukka8@gmail.com>
2023-02-06 17:13:27 +0530

    Update storing and reading an int value Assignment1.sol

--- storing and reading an int value Assignment1.sol
+++ storing and reading an int value Assignment1.sol
@@ -1,8 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
 contract readAndStore{
-    function read(int a) public pure returns(int){
-        int b = a;
-        return b;
+    int stateVar;
+    function read(int a) public  returns(int){
+         stateVar = a;
+        return stateVar;
     }
 } 
